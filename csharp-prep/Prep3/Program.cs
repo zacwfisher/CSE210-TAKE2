5c38ad0bd1a0e4f2b864a4fcc98e5fd443c9aacf
Zac <fis18010@byui.edu>
2025-11-08 20:46:52 -0500

    Prep complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomeGenerator = new Random();
+        int magicNumber = randomeGenerator.Next(1, 101);
+
+        int userGuess = -1;
+
+        while (userGuess != magicNumber)
+        {
+            Console.WriteLine("Guess a number between 1 and 100:");
+            string userInput = Console.ReadLine();
+            userGuess = int.Parse(userInput);
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You've guessed the magic number!");
+            }
+        }
     }
 }
